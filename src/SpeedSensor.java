60e5ba07da4009fe7692fa741868b926bf859ca2
nourhisham <nourhabbas27@gmail.com>
2025-04-28 18:31:28 +0300

    sara esper

--- src/SpeedSensor.java
+++ src/SpeedSensor.java
@@ -1,23 +1,56 @@
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPServiceProviderManager;
+
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class SpeedSensor extends Sensor {
-    private Random random = new Random();
-    private double speedLimit;
+public class SpeedSensor extends Sensor implements Runnable {
+    private static final Random random = new Random();
+    double maxVoltage = 10;
 
-    public SpeedSensor(int id, double speedLimit) {
-        super(id, "Speed");
-        this.speedLimit = speedLimit;
+
+    public SpeedSensor(int id, double lastReading) {
+        super(id, "analog");
+        this.lastReading = lastReading;
     }
 
     @Override
     public double readData() {
-        if(!isActive) return -1;
-        // Simulate speed ±15 km/h around limit
-        lastReading = speedLimit + (random.nextDouble() * 30 - 15);
-        return lastReading;
+        // the reading that gets sent to esper engine
+        if (!isActive) {
+            //if inactive then the speed = 0
+            System.out.println("Sensor inactive");
+            return 0;
+        } else {
+            double voltageReading = measureSpeed();
+            //calculates the speed
+            lastReading = (voltageReading / maxVoltage) * 180;
+            return lastReading;// in voltages
+        }
+
     }
 
-    public double measureSpeed() {
-        return readData();
+    private double measureSpeed(){
+        //mimicking the "real" way of getting speed from a sensor
+        double minVoltage = 0;
+        return minVoltage + (maxVoltage - minVoltage) * random.nextDouble();
+    }
+
+    @Override
+    public void run() {
+        EPServiceProvider engine = EPServiceProviderManager.getDefaultProvider();
+
+        while (true) {
+            engine.getEPRuntime().sendEvent(new SpeedSensor(1, readData()));
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                System.out.println("Error" + ex);
+                Logger.getLogger(SpeedSensor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
+
--- src/TPWSController.java
+++ src/TPWSController.java
@@ -1,7 +1,29 @@
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPStatement;
+
 public class TPWSController {
     private String controllerID;
     private double currentSpeed;
     private double safeDistance;
+
+
+    public Thread EsperRun(EPServiceProvider engine, Sensor sensor){
+        System.out.println("ESPER RUN");
+        String[] sensorString = String.valueOf(sensor).split("@");
+        // Creating EPL statement
+        EPStatement speed_select_statement = engine
+                .getEPAdministrator()
+                .createEPL("select lastReading from " + sensorString[0]);
+
+        // Attaching callback to EPL statements
+        speed_select_statement.setSubscriber(new Object() {
+            public void update(double lastReading) {
+                System.out.println("\n NEW SENSOR READING: " +  lastReading);
+            }
+        });
+
+        return new Thread((Runnable) sensor);
+    }
 
     public void monitorConditions() {
         // Implementation
